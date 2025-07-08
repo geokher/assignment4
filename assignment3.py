fe2041653a93a1add05f890bad25962779c54064
Eben <geokher@gmail.com>
2025-07-08 19:28:47 +0000

    Slicing Names

--- assignment3.py
+++ assignment3.py
@@ -10,3 +10,25 @@ print("type of Name", type(Name))
 print("type of Age", type(Age))
 print("type of Height", type(Height))
 print("type of Is_student", type(Is_student))
+
+
+#Question 3 - Working with variables
+
+x = 5
+y = 2.5
+
+# Their sum
+sum_result = x + y
+print("Sum:", sum_result)
+
+# Their product
+product_result = x * y
+print("Product:", product_result)
+
+# The type of the result of x + y
+print("Type of x + y:", type(sum_result))
+
+#Question 4 - Create a variable called message and assign your full name to it.
+message = "Eben King"
+first_name = message[:4]
+print("First Name:", first_name)
