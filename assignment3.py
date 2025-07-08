243af41eb005244c098a4bc2e44350deeab825df
Eben <geokher@gmail.com>
2025-07-08 19:09:27 +0000

    Print type of variable

--- assignment3.py
+++ assignment3.py
@@ -4,3 +4,9 @@ Age = 29
 Height = 6.1
 Is_student = True
 
+
+#Question 2 - Write a program that prints the type of each variable you created
+print("type of Name", type(Name))
+print("type of Age", type(Age))
+print("type of Height", type(Height))
+print("type of Is_student", type(Is_student))
